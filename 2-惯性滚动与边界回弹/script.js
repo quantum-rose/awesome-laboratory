217b5668e148ebb17556a76a4cec38b9a7a9986c
quantum rose <cui-haojie@outlook.com>
2020-03-08 20:50:35 +0800

    优化代码，添加README.md

--- 2-惯性滚动与边界回弹/script.js
+++ 2-惯性滚动与边界回弹/script.js
@@ -1,19 +1,17 @@
 (function(window, document) {
-    let myScroll = document.querySelector('.my-scroll');
-    let eleScrollLeft = document.querySelector('.scroll-left');
-    let eleScrollTop = document.querySelector('.scroll-top');
-    let eleScrollVelocity = document.querySelector('.scroll-velocity');
-
-    let _scrollLeft = 0;
-    let _scrollTop = 0;
-    let _scrollVelocity = 0;
+    let _eleScrollLeft = document.querySelector('.scroll-left'),
+        _eleScrollTop = document.querySelector('.scroll-top'),
+        _eleScrollVelocity = document.querySelector('.scroll-velocity'),
+        _scrollLeft = 0,
+        _scrollTop = 0,
+        _scrollVelocity = 0;
 
     Object.defineProperty(window, 'scrollLeft', {
         get() {
             return _scrollLeft;
         },
         set(val) {
-            eleScrollLeft.innerHTML = 'scrollLeft: ' + val;
+            _eleScrollLeft.innerHTML = 'scrollLeft: ' + val;
             _scrollLeft = val;
         }
     });
@@ -23,7 +21,7 @@
             return _scrollTop;
         },
         set(val) {
-            eleScrollTop.innerHTML = 'scrollTop: ' + val;
+            _eleScrollTop.innerHTML = 'scrollTop: ' + val;
             _scrollTop = val;
         }
     });
@@ -33,91 +31,125 @@
             return _scrollVelocity;
         },
         set(val) {
-            eleScrollVelocity.innerHTML = 'velocity: ' + val;
+            _eleScrollVelocity.innerHTML = 'velocity: ' + val;
             _scrollVelocity = val;
         }
     });
 
-    let lastPoint = { x: 0, y: 0 }, // 上一次的触点
-        currentPoint = { x: 0, y: 0 }, // 当前触点
+    /**
+     * config 自定义配置
+     */
+    let a = 50, // 滚动时的加速度，总是与滚动方向相反，单位px/(s^2)
+        throttle = 1000 / 60; // 节流延迟时间，用于控制touchmove事件的触发频率，单位ms
+
+    /**
+     * 程序运行所必要的数据
+     */
+    let myScroll = document.querySelector('.my-scroll'),
+        touchIdentifier = null, // touch事件中触点的唯一标识，用于禁用多点触控
+        lastPoint = null, // 上一次的触点
+        currentPoint = null, // 当前触点
         lastTime = 0, // 上一个触点的时间戳
         currentTime = 0, // 当前触点的时间戳
-        a = 50, // 加速度
-        t = 1 / 60, // 惯性滚动的时间间隔
-        timer = null,
-        touchIdentifier = null;
+        isScrolling = false; // 是否正在滚动的标识
 
+    /**
+     * touchstart
+     */
     myScroll.addEventListener('touchstart', function(e) {
         if (!isSingleTouch(e)) {
             return;
         }
+        isScrolling = false;
 
-        if (timer) {
-            clearInterval(timer);
-        }
-
-        let { pageX: x, pageY: y } = e.targetTouches[0];
-        lastPoint = { x, y };
-        currentPoint = { x, y };
-        lastTime = Date.now();
-        currentTime = Date.now();
+        lastPoint = currentPoint = e.targetTouches[0];
+        lastTime = currentTime = Date.now();
     });
 
+    /**
+     * touchmove
+     */
     myScroll.addEventListener('touchmove', function(e) {
-        if (!isSingleTouch(e)) {
+        e.preventDefault();
+
+        if (!isSingleTouch(e) || Date.now() - currentTime < throttle) {
             return;
         }
 
-        e.preventDefault();
-
-        let { pageX: x, pageY: y } = e.targetTouches[0];
         lastPoint = currentPoint;
-        currentPoint = { x, y };
+        currentPoint = e.targetTouches[0];
         lastTime = currentTime;
         currentTime = Date.now();
 
-        scrollLeft += x - lastPoint.x;
-        scrollTop += y - lastPoint.y;
+        scrollLeft += currentPoint.pageX - lastPoint.pageX;
+        scrollTop += currentPoint.pageY - lastPoint.pageY;
         scrollTo(scrollLeft, scrollTop);
     });
 
+    /**
+     * touchend
+     */
     myScroll.addEventListener('touchend', function(e) {
         if (!isSingleTouch(e) || lastTime === currentTime) {
             return;
         }
-        inertialRolling();
+        let deltaX = currentPoint.pageX - lastPoint.pageX,
+            deltaY = currentPoint.pageY - lastPoint.pageY,
+            deltaD = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+            cos = deltaX / deltaD,
+            sin = deltaY / deltaD,
+            scrollVelocity = deltaD / ((currentTime - lastTime) / 1000);
+        isScrolling = true;
+        inertialRolling(cos, sin, scrollVelocity);
     });
 
-    function inertialRolling() {
-        let deltaX = currentPoint.x - lastPoint.x;
-        let deltaY = currentPoint.y - lastPoint.y;
-        let deltaD = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        scrollVelocity = deltaD / ((currentTime - lastTime) / 1000);
-        let cos = deltaX / deltaD;
-        let sin = deltaY / deltaD;
-        timer = setInterval(() => {
-            let v = scrollVelocity - a;
+    /**
+     * 惯性滚动动画
+     * @param {Number} cos 滑动方向与x轴正方向夹角的余弦
+     * @param {Number} sin 滑动方向与x轴正方向夹角的正弦
+     * @param {Number} scrollVelocity 滚动速度，标量，单位px/s
+     */
+    function inertialRolling(cos, sin, scrollVelocity) {
+        if (!isScrolling) {
+            return;
+        }
+        lastTime = currentTime;
+        currentTime = Date.now();
+        let t = (currentTime - lastTime) / 1000;
+        let v = scrollVelocity - a;
 
-            if (v <= 0) {
-                scrollVelocity = 0;
-                clearInterval(timer);
-            } else if (v > 4000) {
-                scrollVelocity = 4000;
-            } else {
-                scrollVelocity = v;
-            }
+        if (v <= 0) {
+            scrollVelocity = 0;
+            isScrolling = false;
+            return;
+        } else if (v > 4000) {
+            scrollVelocity = 4000;
+        } else {
+            scrollVelocity = v;
+        }
 
-            let d = scrollVelocity * t - 0.5 * a * t * t;
-            scrollLeft += cos * d;
-            scrollTop += sin * d;
-            scrollTo(scrollLeft, scrollTop);
-        }, t * 1000);
+        let d = scrollVelocity * t - 0.5 * a * t * t;
+        scrollLeft += cos * d;
+        scrollTop += sin * d;
+        scrollTo(scrollLeft, scrollTop);
+
+        requestAnimationFrame(inertialRolling.bind(this, cos, sin, scrollVelocity));
     }
 
+    /**
+     * 滚动到指定位置
+     * @param {Number} scrollLeft
+     * @param {Number} scrollTop
+     */
     function scrollTo(scrollLeft, scrollTop) {
         myScroll.style.transform = `translate3d(${scrollLeft}px, ${scrollTop}px, 0)`;
     }
 
+    /**
+     * 判断触摸事件是否为单点触控
+     * @param {Event} e Touch Event
+     * @return {Boolean} 是否是单点触控
+     */
     function isSingleTouch(e) {
         if (e.changedTouches) {
             switch (e.type) {
